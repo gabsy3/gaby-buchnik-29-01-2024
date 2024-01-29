f9dcfc455354039266b6efbdfcdf685831b566b4
gaby <gabybuchnik@gmail.com>
2024-01-29 16:22:46 +0200

    weather

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { FavoritesComponent } from './features/favorites/favorites/favorites.component';
+import { WeatherComponent } from './features/weather/weather/weather.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: WeatherComponent},
+    {path: 'favorites', component: FavoritesComponent}
+];
