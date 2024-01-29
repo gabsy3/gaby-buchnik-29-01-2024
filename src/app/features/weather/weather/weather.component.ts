c433287c777c140ed32ba630d7625f4d0eab23a3
gaby <gabybuchnik@gmail.com>
2024-01-29 17:39:27 +0200

    weather

--- src/app/features/weather/weather/weather.component.ts
+++ src/app/features/weather/weather/weather.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-weather',
   standalone: true,
-  imports: [MatInputModule , MatFormFieldModule , MatIconModule , MatButtonModule],
+  imports: [MatInputModule , MatFormFieldModule , MatIconModule , MatButtonModule , CommonModule],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.scss'
 })
-export class WeatherComponent {
-
+export class WeatherComponent implements OnInit{
+  items = []
+  ngOnInit(): void {
+  }
+ 
 }
