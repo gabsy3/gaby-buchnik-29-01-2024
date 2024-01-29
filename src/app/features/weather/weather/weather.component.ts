ed3463ab52d21d8317e98cd201f44e179ae10037
gaby <gabybuchnik@gmail.com>
2024-01-29 17:09:43 +0200

    weather

--- src/app/features/weather/weather/weather.component.ts
+++ src/app/features/weather/weather/weather.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @Component({
   selector: 'app-weather',
   standalone: true,
-  imports: [MatInputModule , MatFormFieldModule , MatIconModule],
+  imports: [MatInputModule , MatFormFieldModule , MatIconModule , MatButtonModule],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.scss'
 })
