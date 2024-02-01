fc5a9eb45587fad617e8c3a27387eca3d517b702
gaby <gabybuchnik@gmail.com>
2024-02-01 23:12:34 +0200

    weather

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -23,13 +23,11 @@ export class WeatherService {
   constructor() {}
 
   getLocation(city: string) {
-    return this.http.get(`http://localhost:3000/location`);
     return this.http.get(
       `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${this.apiKey}&q=${city}`
     );
   }
   getCurrentConditions(locationKey: string) {
-    return this.http.get(`http://localhost:3000/currentConditions`);
     return this.http.get(
       `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${this.apiKey}`
     );
@@ -45,7 +43,6 @@ export class WeatherService {
     }));
   }
   getForecast(locationKey: string) {
-    return this.http.get(`http://localhost:3000/fiveDaysForecasts`);
     return this.http.get(
       `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?metric=true&apikey=${this.apiKey}`
     );
